f28313489a622b04db2bb748e2803d2f7ef90e14
AshuuDixit <72594457+AshuuDixit@users.noreply.github.com>
2021-09-08 17:56:02 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div id="container">
+    <div id="header">© 2021 Made by Ashutosh Dixit <br>
+    ashu.814nfs@gmail.com<br> 
+      </div>
+ </div>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include('nav.php') ?>
+
+
+  <h1>Welcome to BigBuLL's  Bank</h1>
+
+
+  <div class="bg">
+    <div class="center">
+      <ul>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="red"> List of Customers</button></a></li>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">Transfer Money</button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button class="btn" id="green">View Transfer History</button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
