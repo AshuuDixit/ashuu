fc509afc2b8e182b9002260820ee9b56937bc4fe
AshuuDixit <72594457+AshuuDixit@users.noreply.github.com>
2021-09-11 06:21:46 +0530

    commit

--- config.php
+++ config.php
@@ -8,10 +8,10 @@ $db="sparks_bank";
 $conn=mysqli_connect($server,$username,$password,$db);
 
 if($conn){
-  //Connection successfully established
+ 
 }
 
 else
-    die("connection to this database failed due to " .mysqli_connect_error()); //connection not establised
+    die("connection to this database failed due to " .mysqli_connect_error()); 
     
 ?>
